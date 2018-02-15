bb80e06f4047419ddfb9504100b8e0ae9237e8d9
SukachevAlex <sukachevalexandr@gmail.com>
2018-02-15 19:29:39 +0300

    add pattern to inputs, add popup windows

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,49 @@
-var menu = document.querySelector('.menu');
-var hamburger_inner = document.querySelector('.hamburger__inner');
-var nav = document.querySelector('.nav');
-menu.addEventListener('click', function() {
-    hamburger_inner.classList.toggle('is-active');
-    nav.classList.toggle('is-visible');
+
+$( document ).ready(function() {
+    $('.menu').on('click', function() {
+       $('.hamburger__inner').toggleClass('is-active');
+       $('.nav').toggleClass('is-visible');
+    });
+
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+
+    $(".feedback-slider-js").slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        prevArrow: '.feedback-slider__prev',
+        nextArrow: '.feedback-slider__next'
+    });
+
+    $('.consultation-form').on('submit', function() {
+        $(this).hide();
+        $(this).parent().find('.consultation-form_success').show();
+
+        return false;
+    });
+
+    $('.consultation-form__close').on('click', function() {
+        $('.consultation-form_success').hide();
+        $('.consultation-form').show();
+        if ($(this).parent('.consultation-form__wrapper').length) {
+            $.magnificPopup.close();
+        }
+
+    });
+
+    $('.popup-link').magnificPopup({
+        type:'inline',
+        midClick: true
+    });
+
+    $('#popup__close').on('click', function() {
+        $.magnificPopup.close();
+    })
 });
-
-
-$(".feedback-slider-js").slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    prevArrow: '.feedback-slider__prev',
-    nextArrow: '.feedback-slider__next'
-});
-
--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -2,7 +2,8 @@ module.exports = function () {
 
     $.gulp.task('libs:dev', function(){
         return $.gulp.src([
-            'node_modules/slick-carousel/slick/slick.js'
+            'node_modules/slick-carousel/slick/slick.js',
+            'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
         ])
             .pipe($.gp.concat('libs.min.js'))
             .pipe($.gulp.dest('dist/js/'))
@@ -38,6 +39,8 @@ module.exports = function () {
                 stream: true
             }));
     });
+
+
 
 };
 
